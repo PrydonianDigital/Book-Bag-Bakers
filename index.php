6b07d3ff3c862b43d1a47a23a9577f8b3b802229
vbloke <vbloke@gmail.com>
2015-04-06 08:47:49 +0100

    clean up

--- index.php
+++ index.php
@@ -6,35 +6,46 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div <?php post_class('blogPost'); ?>>
 		<div class="row" itemscope itemtype="http://schema.org/BlogPosting">
-			
+
 			<div class="twelve columns">
-				
+
 				<h3 itemprop="name headline" class="entry-title p-name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 				<h5><span datetime="<?php the_time( 'c' ); ?>" itemprop="datePublished" class="updated dtstart"><?php the_time( 'D, jS M, Y' ); ?> / by <span class="vcard author"><span class="fn"><?php the_author('nicename'); ?></span></span> <small>(<?php comments_number( 'no comments', '1 comment', '% comments' ); ?>)</small></h5>
-			</div>			
+			</div>
 		</div>
-		
+
 		<div class="row">
-			
+
+			<?php if ( has_post_thumbnail() ) { ?>
 			<div class="three columns">
-				
+
 				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('article'); ?></a>
-				
+
 			</div>
-			
+
 			<div class="nine columns p-summary" itemprop="articleBody">
-				
+
 				<?php the_excerpt(); ?>
-				
+
 				<div class="woocommerce"><a href="<?php the_permalink(); ?>" class="button wc-forward">Read...</a></div>
-				
+
 			</div>
-			
+			<?php } else { ?>
+
+			<div class="twelve columns p-summary" itemprop="articleBody">
+
+				<?php the_excerpt(); ?>
+
+				<div class="woocommerce"><a href="<?php the_permalink(); ?>" class="button wc-forward">Read...</a></div>
+
+			</div>
+			<?php } ?>
+
 		</div>
 	</div>
-	
+
 	<?php endwhile; ?>
-	
+
 	<?php endif; ?>
 
 	</div>
